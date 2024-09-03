4df2a03ea092112a4cbd3b7cfeb20581df359125
clrn <clrnmail@gmail.com>
2024-09-03 10:21:04 +0100

    refactor

--- lua/gdbdisasm.lua
+++ lua/gdbdisasm.lua
@@ -3,9 +3,10 @@ local async = require("plenary.async")
 local ts_utils = require("nvim-treesitter.ts_utils")
 
 local ns_id = vim.api.nvim_create_namespace("disnav")
-LAST_DISASM_LINES = {}
-COMMS = nil
-LAST_PATH = ""
+local last_disasm_lines = {}
+local comms = nil
+local last_path = ""
+local last_notification = nil
 
 local function get_current_function_range()
 	local current_node = ts_utils.get_node_at_cursor()
@@ -51,7 +52,7 @@ local function start_gdb()
 	job:start()
 
 	local function communicate(command)
-		vim.print("writing '" .. command .. "'")
+		-- vim.print("writing '" .. command .. "'")
 		job:send(command .. "\n")
 
 		local lines = {}
@@ -84,45 +85,69 @@ local function make_sure_gdb_is_running()
 
 	local path = cmake.get_launch_path(target) .. target
 
-	if not COMMS then
-		COMMS = start_gdb()
+	if not comms then
+		comms = start_gdb()
 
 		-- set some settings
-		COMMS("set disassembly-flavor intel")
-		COMMS("set print asm-demangle")
+		comms("set disassembly-flavor intel")
+		comms("set print asm-demangle")
 	end
 
 	-- load file
-	if path ~= LAST_PATH then
-		LAST_PATH = path
-		COMMS(string.format("file %s", path))
+	if path ~= last_path then
+		last_path = path
+		comms(string.format("file %s", path))
 	end
+end
+
+---Renders the disasm text
+---@param buf_id integer
+---@param lines table
+local function draw_disasm_lines(buf_id, lines)
+	vim.schedule(function()
+		vim.api.nvim_buf_clear_namespace(buf_id, ns_id, 0, -1)
+
+		for line_num, text in pairs(lines) do
+			local virt_lines = {}
+			for _, line in ipairs(text) do
+				-- vim.print(string.format("line num: %s, line text: %s", line_num, line))
+				table.insert(virt_lines, { { "    " .. line, "Comment" } })
+			end
+
+			local col_num = 0
+			vim.api.nvim_buf_set_extmark(buf_id, ns_id, line_num - 1, col_num, {
+				virt_lines = virt_lines,
+			})
+		end
+		vim.notify(string.format("Disasm text is drawn"), vim.log.levels.INFO)
+	end)
 end
 
 local function disasm_current_func()
 	local cur_file = vim.fn.expand("%:p")
 	local line_start, line_end = get_current_function_range()
+	---@diagnostic disable-next-line: deprecated
 	local cursor_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
 	async.run(function()
 		make_sure_gdb_is_running()
 
 		-- fetch what's the function address for the given lines
-		local info = COMMS(string.format("info line %s:%s", cur_file, cursor_line))
+		local info = comms(string.format("info line %s:%s", cur_file, cursor_line))
 		local func_addr = string.match(info[1], "0x[0-9a-h]+")
 
 		-- disasm function address
-		local response = COMMS(string.format("disassemble %s", func_addr))
+		local response = comms(string.format("disassemble %s", func_addr))
 
 		local disasm = {}
 		local last_line = 1
 
-		LAST_DISASM_LINES = {}
+		last_disasm_lines = {}
 
 		for _, str in ipairs(response) do
 			local addr, asm = string.match(str, "^%s+(0x[0-9a-h]+)%s+<[^>]+>:([^\n]+)")
 			if addr and asm then
-				local t = COMMS(string.format("list *%s", addr))
+				local t = comms(string.format("list *%s", addr))
 				local raw = t[1]
 				local last = #raw - raw:reverse():find(" ") + 1
 				local s = raw:sub(last + 2, -4)
@@ -141,7 +166,7 @@ local function disasm_current_func()
 					disasm[last_line] = {}
 				end
 
-				table.insert(LAST_DISASM_LINES, string.format("%-90s // %s:%s", asm, file, last_line))
+				table.insert(last_disasm_lines, string.format("%-90s // %s:%s", asm, file, last_line))
 
 				if last_line <= line_end + 1 and last_line >= line_start then
 					table.insert(disasm[last_line], asm)
@@ -149,32 +174,20 @@ local function disasm_current_func()
 			end
 		end
 
-		vim.schedule(function()
-			for line_num, text in pairs(disasm) do
-				local virt_lines = {}
-				for _, line in ipairs(text) do
-					-- vim.print(string.format("line num: %s, line text: %s", line_num, line))
-					table.insert(virt_lines, { { "    " .. line, "Comment" } })
-				end
-
-				local col_num = 0
-				vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, col_num, {
-					virt_lines = virt_lines,
-				})
-			end
-		end)
+		draw_disasm_lines(0, disasm)
 	end)
 end
 
 local function resolve_calls_under_the_cursor()
 	local cur_file = vim.fn.expand("%:p")
+	---@diagnostic disable-next-line: deprecated
 	local cursor_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
 	async.run(function()
 		make_sure_gdb_is_running()
 
 		-- fetch what's the function address for the given lines
-		local info = COMMS(string.format("info line %s:%s", cur_file, cursor_line))
+		local info = comms(string.format("info line %s:%s", cur_file, cursor_line))
 
 		local match = string.gmatch(info[1], "0x[0-9a-h]+")
 		local addresses = {}
@@ -183,12 +196,12 @@ local function resolve_calls_under_the_cursor()
 		end
 
 		-- disasm function address
-		local response = COMMS(string.format("disassemble %s,%s", addresses[1], addresses[2]))
+		local response = comms(string.format("disassemble %s,%s", addresses[1], addresses[2]))
 		local names = {}
 		local jumps = {}
 		for _, str in ipairs(response) do
 			local _, asm = string.match(str, "^%s+(0x[0-9a-h]+)%s+<[^>]+>:([^\n]+)")
-			vim.print(asm)
+			-- vim.print(asm)
 			if asm and asm:find("call ") then
 				local addr, name = string.match(asm, "^[^0]+(0x[0-9a-h]+)%s+([^\n]+)")
 				if name then
@@ -219,7 +232,7 @@ local function resolve_calls_under_the_cursor()
 			addr = rx()
 		end
 
-		info = COMMS(string.format("info line *%s", addr))
+		info = comms(string.format("info line *%s", addr))
 		local line, file = string.match(info[1], 'Line%s(%d+)[^"]+["]([^"]+)["]')
 
 		vim.schedule(function()
@@ -238,20 +251,20 @@ M.setup = function(cfg)
 
 	vim.keymap.set("n", "<leader>daq", function()
 		vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
-		if COMMS then
-			local c = COMMS
+		if comms then
+			local c = comms
 
 			async.run(function()
 				c(string.format("quit"))
 			end)
 
-			COMMS = nil
-			LAST_PATH = ""
+			comms = nil
+			last_path = ""
 		end
 	end, { remap = true, desc = "Clean disassembly and quit GDB" })
 
 	vim.keymap.set("n", "<leader>das", function()
-		vim.api.nvim_buf_set_lines(0, 0, -1, false, LAST_DISASM_LINES)
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, last_disasm_lines)
 	end, { remap = true, desc = "Set disassembly text to current buffer" })
 
 	vim.keymap.set("n", "<leader>dac", function()

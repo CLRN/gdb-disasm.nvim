65e00a96be9fc7423dd1c42aaba616c05e0d3a7f
clrn <clrnmail@gmail.com>
2024-09-03 09:48:24 +0100

    reformat

--- lua/gdbdisasm.lua
+++ lua/gdbdisasm.lua
@@ -1,262 +1,262 @@
-local Job = require 'plenary.job'
-local async = require "plenary.async"
-local ts_utils = require'nvim-treesitter.ts_utils'
+local Job = require("plenary.job")
+local async = require("plenary.async")
+local ts_utils = require("nvim-treesitter.ts_utils")
 
-local ns_id = vim.api.nvim_create_namespace "disnav"
+local ns_id = vim.api.nvim_create_namespace("disnav")
 LAST_DISASM_LINES = {}
 COMMS = nil
 LAST_PATH = ""
 
 local function get_current_function_range()
-  local current_node = ts_utils.get_node_at_cursor()
-  local expr = current_node
+	local current_node = ts_utils.get_node_at_cursor()
+	local expr = current_node
 
-  while expr do
-    if expr:type() == 'function_definition' then
-      break
-    end
-    expr = expr:parent()
-  end
+	while expr do
+		if expr:type() == "function_definition" then
+			break
+		end
+		expr = expr:parent()
+	end
 
-  if not expr then return 0, 0 end
-  local range = vim.treesitter.get_range(expr)
-  return range[1], range[4]
+	if not expr then
+		return 0, 0
+	end
+	local range = vim.treesitter.get_range(expr)
+	return range[1], range[4]
 end
 
 local function start_gdb()
-  local setter, getter = async.control.channel.mpsc()
-  local job = Job:new({
-    command = "gdb",
-    args = { "--interpreter", "mi" },
+	local setter, getter = async.control.channel.mpsc()
+	local job = Job:new({
+		command = "gdb",
+		args = { "--interpreter", "mi" },
 
-    on_stdout = function(err, line)
-      setter.send(line)
-    end,
+		on_stdout = function(err, line)
+			setter.send(line)
+		end,
 
-    on_stderr = function(err, line)
-      vim.notify(string.format("GDB error %s", line), vim.log.levels.ERROR)
-    end,
+		on_stderr = function(err, line)
+			vim.notify(string.format("GDB error %s", line), vim.log.levels.ERROR)
+		end,
 
-    on_exit = function(j, return_val)
-      if return_val ~= 0 then
-        vim.notify(string.format("GDB terminated with %s", return_val), vim.log.levels.ERROR)
-      else
-        vim.notify(string.format("GDB terminated cleanly"), vim.log.levels.INFO)
-      end
-    end,
-  })
+		on_exit = function(j, return_val)
+			if return_val ~= 0 then
+				vim.notify(string.format("GDB terminated with %s", return_val), vim.log.levels.ERROR)
+			else
+				vim.notify(string.format("GDB terminated cleanly"), vim.log.levels.INFO)
+			end
+		end,
+	})
 
-  job:start()
+	job:start()
 
-  local function communicate(command)
-    vim.print("writing '" .. command .. "'")
-    job:send(command .. "\n")
+	local function communicate(command)
+		vim.print("writing '" .. command .. "'")
+		job:send(command .. "\n")
 
-    local lines = {}
-    while true do
-      local line = getter.recv()
-      if string.sub(line, 1, 1) == "^" then
-        return lines
-      end
+		local lines = {}
+		while true do
+			local line = getter.recv()
+			if string.sub(line, 1, 1) == "^" then
+				return lines
+			end
 
-      if string.sub(line, 1, 1) == "~" then
-        table.insert(lines, vim.json.decode(string.sub(line, 2)))
-      end
-    end
-  end
+			if string.sub(line, 1, 1) == "~" then
+				table.insert(lines, vim.json.decode(string.sub(line, 2)))
+			end
+		end
+	end
 
-  return communicate
+	return communicate
 end
 
 local function make_sure_gdb_is_running()
-  local status, cmake = pcall(require, "cmake-tools")
-  if not status then
-    vim.notify("unable to load cmake-tools", vim.log.levels.ERROR)
-    return
-  end
-  local target = cmake.get_launch_target()
-  if not target then
-    vim.notify("unable to load cmake target", vim.log.levels.ERROR)
-    return
-  end
+	local status, cmake = pcall(require, "cmake-tools")
+	if not status then
+		vim.notify("unable to load cmake-tools", vim.log.levels.ERROR)
+		return
+	end
+	local target = cmake.get_launch_target()
+	if not target then
+		vim.notify("unable to load cmake target", vim.log.levels.ERROR)
+		return
+	end
 
-  local path = cmake.get_launch_path(target) .. target
+	local path = cmake.get_launch_path(target) .. target
 
-  if not COMMS then
-    COMMS = start_gdb()
+	if not COMMS then
+		COMMS = start_gdb()
 
-    -- set some settings
-    COMMS("set disassembly-flavor intel")
-    COMMS("set print asm-demangle")
-  end
+		-- set some settings
+		COMMS("set disassembly-flavor intel")
+		COMMS("set print asm-demangle")
+	end
 
-  -- load file
-  if path ~= LAST_PATH then
-    LAST_PATH = path
-    COMMS(string.format("file %s", path))
-  end
+	-- load file
+	if path ~= LAST_PATH then
+		LAST_PATH = path
+		COMMS(string.format("file %s", path))
+	end
 end
 
 local function disasm_current_func()
-  local cur_file = vim.fn.expand('%:p')
-  local line_start, line_end = get_current_function_range()
-  local cursor_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+	local cur_file = vim.fn.expand("%:p")
+	local line_start, line_end = get_current_function_range()
+	local cursor_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
-  async.run(function()
-    make_sure_gdb_is_running()
+	async.run(function()
+		make_sure_gdb_is_running()
 
-    -- fetch what's the function address for the given lines
-    local info = COMMS(string.format("info line %s:%s", cur_file, cursor_line))
-    local func_addr = string.match(info[1], "0x[0-9a-h]+")
+		-- fetch what's the function address for the given lines
+		local info = COMMS(string.format("info line %s:%s", cur_file, cursor_line))
+		local func_addr = string.match(info[1], "0x[0-9a-h]+")
 
-    -- disasm function address
-    local response = COMMS(string.format("disassemble %s", func_addr))
+		-- disasm function address
+		local response = COMMS(string.format("disassemble %s", func_addr))
 
-    local disasm = {}
-    local last_line = 1
+		local disasm = {}
+		local last_line = 1
 
-    LAST_DISASM_LINES = {}
+		LAST_DISASM_LINES = {}
 
-    for _, str in ipairs(response) do
-      local addr, asm = string.match(str, "^%s+(0x[0-9a-h]+)%s+<[^>]+>:([^\n]+)")
-      if addr and asm then
-        local t = COMMS(string.format("list *%s", addr))
-        local raw = t[1]
-        local last = #raw - raw:reverse():find(" ") + 1
-        local s = raw:sub(last + 2, -4)
-        local delim = s:find(":")
+		for _, str in ipairs(response) do
+			local addr, asm = string.match(str, "^%s+(0x[0-9a-h]+)%s+<[^>]+>:([^\n]+)")
+			if addr and asm then
+				local t = COMMS(string.format("list *%s", addr))
+				local raw = t[1]
+				local last = #raw - raw:reverse():find(" ") + 1
+				local s = raw:sub(last + 2, -4)
+				local delim = s:find(":")
 
-        local file = s:sub(1, delim - 1)
-        local line = tonumber(s:sub(delim + 1))
-        if line and cur_file == file then
-          last_line = line
-        else
-          asm = string.format("%-90s // %s:%s", asm, file, line)
-          -- vim.print(string.format("unmatched file %s against %s", t[1], cur_file))
-        end
+				local file = s:sub(1, delim - 1)
+				local line = tonumber(s:sub(delim + 1))
+				if line and cur_file == file then
+					last_line = line
+				else
+					asm = string.format("%-90s // %s:%s", asm, file, line)
+					-- vim.print(string.format("unmatched file %s against %s", t[1], cur_file))
+				end
 
-        if not disasm[last_line] then
-          disasm[last_line] = {}
-        end
+				if not disasm[last_line] then
+					disasm[last_line] = {}
+				end
 
-        table.insert(LAST_DISASM_LINES, string.format("%-90s // %s:%s", asm, file, last_line))
+				table.insert(LAST_DISASM_LINES, string.format("%-90s // %s:%s", asm, file, last_line))
 
-        if last_line <= line_end + 1 and last_line >= line_start then
-          table.insert(disasm[last_line], asm)
-        end
-      end
-    end
+				if last_line <= line_end + 1 and last_line >= line_start then
+					table.insert(disasm[last_line], asm)
+				end
+			end
+		end
 
-    vim.schedule(function ()
-      for line_num, text in pairs(disasm) do
-        local virt_lines = {}
-        for _, line in ipairs(text) do
-          -- vim.print(string.format("line num: %s, line text: %s", line_num, line))
-          table.insert(virt_lines, { { "    " .. line, "Comment" } })
-        end
+		vim.schedule(function()
+			for line_num, text in pairs(disasm) do
+				local virt_lines = {}
+				for _, line in ipairs(text) do
+					-- vim.print(string.format("line num: %s, line text: %s", line_num, line))
+					table.insert(virt_lines, { { "    " .. line, "Comment" } })
+				end
 
-        local col_num = 0
-        vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, col_num, {
-          virt_lines = virt_lines,
-        })
-      end
-    end)
-
-  end)
+				local col_num = 0
+				vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, col_num, {
+					virt_lines = virt_lines,
+				})
+			end
+		end)
+	end)
 end
 
 local function resolve_calls_under_the_cursor()
-  local cur_file = vim.fn.expand('%:p')
-  local cursor_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+	local cur_file = vim.fn.expand("%:p")
+	local cursor_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
-  async.run(function()
-    make_sure_gdb_is_running()
+	async.run(function()
+		make_sure_gdb_is_running()
 
-    -- fetch what's the function address for the given lines
-    local info = COMMS(string.format("info line %s:%s", cur_file, cursor_line))
+		-- fetch what's the function address for the given lines
+		local info = COMMS(string.format("info line %s:%s", cur_file, cursor_line))
 
-    local match = string.gmatch(info[1], "0x[0-9a-h]+")
-    local addresses = {}
-    for i in match do
-      table.insert(addresses, i)
-    end
+		local match = string.gmatch(info[1], "0x[0-9a-h]+")
+		local addresses = {}
+		for i in match do
+			table.insert(addresses, i)
+		end
 
-    -- disasm function address
-    local response = COMMS(string.format("disassemble %s,%s", addresses[1], addresses[2]))
-    local names = {}
-    local jumps = {}
-    for _, str in ipairs(response) do
-      local _, asm = string.match(str, "^%s+(0x[0-9a-h]+)%s+<[^>]+>:([^\n]+)")
-      vim.print(asm)
-      if asm and asm:find("call ")  then
-        local addr, name = string.match(asm, "^[^0]+(0x[0-9a-h]+)%s+([^\n]+)")
-        if name then
-          table.insert(names, name)
-          table.insert(jumps, addr)
-        end
-      end
-    end
+		-- disasm function address
+		local response = COMMS(string.format("disassemble %s,%s", addresses[1], addresses[2]))
+		local names = {}
+		local jumps = {}
+		for _, str in ipairs(response) do
+			local _, asm = string.match(str, "^%s+(0x[0-9a-h]+)%s+<[^>]+>:([^\n]+)")
+			vim.print(asm)
+			if asm and asm:find("call ") then
+				local addr, name = string.match(asm, "^[^0]+(0x[0-9a-h]+)%s+([^\n]+)")
+				if name then
+					table.insert(names, name)
+					table.insert(jumps, addr)
+				end
+			end
+		end
 
-    if #names == 0 then
-      return
-    end
+		if #names == 0 then
+			return
+		end
 
-    local tx, rx = async.control.channel.oneshot()
-    local addr = nil
+		local tx, rx = async.control.channel.oneshot()
+		local addr = nil
 
-    if #names == 1 then
-      addr = jumps[1]
-    else
-      vim.schedule(function ()
-        vim.ui.select(names, {
-          prompt = 'Pick function to jump to',
-        }, function(choice, idx)
-            tx(jumps[idx])
-          end)
-      end)
+		if #names == 1 then
+			addr = jumps[1]
+		else
+			vim.schedule(function()
+				vim.ui.select(names, {
+					prompt = "Pick function to jump to",
+				}, function(choice, idx)
+					tx(jumps[idx])
+				end)
+			end)
 
-      addr = rx()
-    end
+			addr = rx()
+		end
 
-    info = COMMS(string.format("info line *%s", addr))
-    local line, file = string.match(info[1], "Line%s(%d+)[^\"]+[\"]([^\"]+)[\"]")
+		info = COMMS(string.format("info line *%s", addr))
+		local line, file = string.match(info[1], 'Line%s(%d+)[^"]+["]([^"]+)["]')
 
-    vim.schedule(function ()
-      vim.cmd(string.format(":edit %s", file))
-      vim.cmd(string.format(":%d", line))
-    end)
-  end)
+		vim.schedule(function()
+			vim.cmd(string.format(":edit %s", file))
+			vim.cmd(string.format(":%d", line))
+		end)
+	end)
 end
-
 
 M = {}
 
 M.setup = function(cfg)
-  vim.keymap.set("n", "<leader>daf", function()
-    disasm_current_func()
-  end, { remap = true, desc = "Disassemble current function" })
+	vim.keymap.set("n", "<leader>daf", function()
+		disasm_current_func()
+	end, { remap = true, desc = "Disassemble current function" })
 
-  vim.keymap.set("n", "<leader>daq", function()
-    vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
-    if COMMS then
-      local c = COMMS
+	vim.keymap.set("n", "<leader>daq", function()
+		vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+		if COMMS then
+			local c = COMMS
 
-      async.run(function()
-        c(string.format("quit"))
-      end)
+			async.run(function()
+				c(string.format("quit"))
+			end)
 
-      COMMS = nil
-      LAST_PATH = ""
-    end
-  end, { remap = true, desc = "Clean disassembly and quit GDB" })
+			COMMS = nil
+			LAST_PATH = ""
+		end
+	end, { remap = true, desc = "Clean disassembly and quit GDB" })
 
-  vim.keymap.set("n", "<leader>das", function()
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, LAST_DISASM_LINES)
-  end, { remap = true, desc = "Set disassembly text to current buffer" })
+	vim.keymap.set("n", "<leader>das", function()
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, LAST_DISASM_LINES)
+	end, { remap = true, desc = "Set disassembly text to current buffer" })
 
-  vim.keymap.set("n", "<leader>dac", function()
-    resolve_calls_under_the_cursor()
-  end, { remap = true, desc = "Jump to a call" })
+	vim.keymap.set("n", "<leader>dac", function()
+		resolve_calls_under_the_cursor()
+	end, { remap = true, desc = "Jump to a call" })
 end
 
 return M

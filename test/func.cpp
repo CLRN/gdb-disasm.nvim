23f1915f19c38f2cd60e3d5ac4e7dda30e50ec8b
clrn <clrnmail@gmail.com>
2024-09-11 11:24:29 +0000

    clean tests

--- test/func.cpp
+++ test/func.cpp
@@ -1,6 +1,7 @@
 
 int calc(int x, int y)
 {
+    // this is a simple function we can disasemble
     volatile int val = 0;
     return x + y + val;
 }
--- test/perf.cpp
+++ test/perf.cpp
@@ -1,40 +1,16 @@
 #include "func.h"
 #include <benchmark/benchmark.h>
-#include <string>
 
+void bm_book(benchmark::State &state) {
+  size_t test = 0;
 
-template <typename MatcherType>
-void bm_book(benchmark::State& state)
-{
-    size_t test = 0;
+  // another use case is a quick asm diff
+  for (int i = 0; i < 100; ++i) {
+    test += 0;
     test = calc(test, test + 1);
-    std::string s(test, 'a');
-    s.size();
-    // for (auto _ : state) {
-    // }
-
-    // state.SetItemsProcessed(static_cast<int64_t>(state.iterations()) *
-    //                         messages.size());
-    state.counters["updates"] = test;
+  }
 }
 
-BENCHMARK(bm_book<int>)->Unit(benchmark::kMillisecond)->Repetitions(1);
+BENCHMARK(bm_book)->Unit(benchmark::kMillisecond)->Repetitions(1);
 
 BENCHMARK_MAIN();
-
-// #include <string>
-//
-//
-// int main(int argc, char** argv) {
-//
-//     size_t test = 1;
-//
-//     test = calc(test, test + 1);
-//
-//     // test
-//     test = calc2(test);
-//
-//     std::string s(argc, 'a');
-//     return s.size();
-// }
-//
